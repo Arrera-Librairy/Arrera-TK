e22e280c0183e005a58a3f6c38068211de0a098a
Baptiste P <baptiste.pauchet13@gmail.com>
2024-10-31 22:24:58 +0100

    Teste du constructeur

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from arrera_tk.arrera_tk import *
 
 def main():
+    CArreraTK() # This will create a Tkinter window with the title "Super windows" and resizable
     pass
 
 if __name__ == '__main__':
