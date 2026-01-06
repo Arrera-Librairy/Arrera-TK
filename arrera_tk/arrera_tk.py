8aa9902ac71183a724858042742051c996cf193e
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-01-06 15:45:47 +0100

    Reorganisation

--- arrera_tk/arrera_tk.py
+++ arrera_tk/arrera_tk.py
@@ -13,6 +13,23 @@ def resource_path(relative_path):
     else:
         return relative_path
 
+# Widget
+
+class aLabel(ctk.CTkLabel):
+    def __init__(self, master, text: str = "Arrera Label", **kwargs):
+        super().__init__(master, text=text, **kwargs)
+
+class aButton(ctk.CTkButton):
+    def __init__(self, master, text: str = "Arrera Button", width: int = 140, height: int = 40, command=None, **kwargs):
+        super().__init__(master, text=text, width=width, height=height, command=command, **kwargs)
+
+class aFrame(ctk.CTkFrame):
+    def __init__(self, master,corner_radius: int = 20, **kwargs):
+        super().__init__(master, **kwargs)
+        self.configure(corner_radius=corner_radius)
+
+# Fenetre
+
 class aTk(ctk.CTk):
     def __init__(self, title: str = "ArreraTK", width: int = 800, height: int = 600, resizable: bool = False, icon: str = "",theme_file:str="theme/theme_material3.json"):
         super().__init__()
@@ -48,16 +65,3 @@ class aTopLevel(ctk.CTkToplevel):
                     self.iconbitmap(icon)
             elif os.path.splitext(icon)[1].lower() == '.png' :
                 self.iconphoto(True, PhotoImage(file=icon))
-
-class aLabel(ctk.CTkLabel):
-    def __init__(self, master, text: str = "Arrera Label", **kwargs):
-        super().__init__(master, text=text, **kwargs)
-
-class aButton(ctk.CTkButton):
-    def __init__(self, master, text: str = "Arrera Button", width: int = 140, height: int = 40, command=None, **kwargs):
-        super().__init__(master, text=text, width=width, height=height, command=command, **kwargs)
-
-class aFrame(ctk.CTkFrame):
-    def __init__(self, master,corner_radius: int = 20, **kwargs):
-        super().__init__(master, **kwargs)
-        self.configure(corner_radius=corner_radius)
