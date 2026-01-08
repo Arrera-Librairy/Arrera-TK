b40568c0c7c114647bf231ddd656f1963031d8d8
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-01-08 14:51:19 +0100

    Ajout de fonction supplementaire a la texte box

--- arrera_tk/arrera_tk.py
+++ arrera_tk/arrera_tk.py
@@ -65,6 +65,21 @@ class aText(ctk.CTkTextbox):
             self._textbox.tag_configure("center", justify="center")
             self._textbox.tag_add("center", "0.0", "end")
 
+    def disable_textbox(self):
+        self.configure(state="disabled")
+
+    def enable_textbox(self):
+        self.configure(state="normal")
+        self.focus()
+
+    def insert_text(self,text:str):
+        self.configure(state="normal")
+        self.insert("1.0",text)
+        self.configure(state="disabled")
+
+    def centre_text(self):
+        self._textbox.tag_configure("center", justify="center")
+
 class aTextScrollable(ctk.CTkFrame):
     def __init__(self,master):
         super().__init__(master)
